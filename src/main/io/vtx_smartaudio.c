6cf0500427bd8d9e68597215d8bb177a185dc950
Tuomas Kuosmanen <tigert@redhat.com>
2017-12-03 19:06:49 +0200

    revert the serial fixes that break AKK vide otranmitters

--- src/main/io/vtx_smartaudio.c
+++ src/main/io/vtx_smartaudio.c
@@ -679,7 +679,7 @@ bool vtxSmartAudioInit(void)
 
     serialPortConfig_t *portConfig = findSerialPortConfig(FUNCTION_VTX_SMARTAUDIO);
     if (portConfig) {
-        portOptions_e portOptions = SERIAL_STOPBITS_2;
+        portOptions_e portOptions = 0;
 #if defined(VTX_COMMON)
         portOptions = portOptions | (vtxConfig()->halfDuplex ? SERIAL_BIDIR | SERIAL_BIDIR_PP : SERIAL_UNIDIR);
 #else
